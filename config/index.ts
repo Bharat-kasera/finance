6e5619d85e3e9c93dce7734704f0463ee424ca58
Bharat Kasera <bharat.codewolf@gmail.com>
2024-06-10 13:48:42 +0530

    first commit

--- config/index.ts
+++ config/index.ts
@@ -45,11 +45,11 @@ export const siteConfig: Metadata = {
     "css",
   ] as Array<string>,
   authors: {
-    name: "Sanidhya Kumar Verma",
-    url: "https://github.com/sanidhyy",
+    name: "Bharat Kasera",
+    url: "https://github.com/Bharat-kasera",
   },
 } as const;
 
 export const links = {
-  sourceCode: "https://github.com/sanidhyy/finance-dashboard",
+  sourceCode: "https://github.com/Bharat-kasera/finance",
 } as const;
